3119b534ffd7d109ed8ce8112523af73435bab52
Secret1000 <141339442+Glennreble@users.noreply.github.com>
2023-10-06 16:20:14 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -99,3 +99,42 @@ addLayer("a", {
         ]
     }, 
 )
+addLayer("y", {
+    name: "Yfn", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "Y", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#FDFDFD",
+    requires: new Decimal(1e8), // Can be a function that takes requirement increases into account
+    resource: "yfn", // Name of prestige currency
+    baseResource: "rocket power", // Name of resource prestige is based on
+    baseAmount() {return player.r.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true},
+ upgrades: {
+        11: {
+        title: "Yet another layer",
+        description: "YFN boost your distance gain.",
+        cost: new Decimal(1),
+        effect() {
+            return player[this.layer].points.add(1).pow(0.5)
+        },
+        effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
+        }
+   }
+})
