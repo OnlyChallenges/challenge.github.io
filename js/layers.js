f1c768a1630e51928b2855c3d4869ad5dccf833a
Secret1000 <141339442+Glennreble@users.noreply.github.com>
2023-10-06 16:42:33 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -22,7 +22,7 @@ addLayer("r", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "r", description: "R: Reset for rocket power", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true},
  upgrades: {
@@ -127,9 +127,10 @@ addLayer("y", {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#00FFFF",
+    color: "#808080",
     requires: new Decimal(1e8), // Can be a function that takes requirement increases into account
     resource: "yfn", // Name of prestige currency
+    branches: ["r"],
     baseResource: "rockek power", // Name of resource prestige is based on
     baseAmount() {return player.r.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
