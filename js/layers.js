83ab0766254c97f65107d29a75a36db36904373d
Secret1000 <141339442+Glennreble@users.noreply.github.com>
2023-10-10 09:08:13 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -140,7 +140,7 @@ addLayer("y", {
     symbol: "Y", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
-        unlocked: true,
+        unlocked: false,
 		points: new Decimal(0),
     }},
     color: "lime",
@@ -183,4 +183,36 @@ addLayer("y", {
         effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
         }
    }
+})
+addLayer("s", {
+name: "sacrifice",
+symbol: "S",
+position: 0,
+    startData() { return {                  // startData is a function that returns default data for a layer. 
+        unlocked: false,                     // You can add more variables here to add them to your layer.
+        points: new Decimal(0),             // "points" is the internal name for the main resource of the layer.
+    }},
+
+    color: "gold",                       // The color for this layer, which affects many elements.
+    resource: "sacrifice points",            // The name of this layer's main prestige resource.
+    row: 2,                                 // The row this layer is on (0 is the first row).
+
+    baseResource: "yfn",   // The name of the resource your prestige gain is based on.
+    branches: ["y"]
+    baseAmount() { return player.y.points },  // A function to return the current amount of baseResource.
+    requires: new Decimal(1.5e30),              // The amount of the base needed to  gain 1 of the prestige currency                                            // Also the amount required to unlock the laye
+    type: "normal",                         // Determines the formula used for calculating prestige currency.
+    exponent: 0.7,                          // "normal" prestige gain is (currency^exponent).
+
+    gainMult() {                            // Returns your multiplier to your gain of the prestige resource.
+        return new Decimal(1)               // Factor in any bonuses multiplying gain here.
+    },
+    gainExp() {                             // Returns the exponent to your gain of the prestige resource.
+        return new Decimal(1)
+    },
+
+    layerShown() { return player.y.points.gte(new Decimal(1e30))||player.s.unlocked},          // Returns a bool for if this layer's node should be visible in the tre
+ upgrades: {
+        // Look in the upgrades docs to see what goes here!
+    },
 })
