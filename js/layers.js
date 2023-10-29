2a1a2043e349f31fd61666dd5c0bb147d3357d29
Acythe3 <marcusjacksondred@gmail.com>
2023-10-28 20:59:49 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -111,7 +111,7 @@ addLayer("c", {
             description: "There's Honey Crystals around the corner-- infects are increased",
             cost: new Decimal(24500),
             effect() {
-                return player[this.layer].points.add(1).pow(1.2)
+                return player[this.layer].points.add(1).pow(0.11)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
             },
