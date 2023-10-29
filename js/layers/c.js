04e81e214718fe4a7a1393eaf17c7d8f3bcbf4e9
Acythe3 <marcusjacksondred@gmail.com>
2023-10-29 00:10:19 -0400

    Update c.js

--- js/layers/c.js
+++ js/layers/c.js
@@ -40,7 +40,7 @@ addLayer("c", {
         description: "Crystals boosts infects slightly",
         cost: new Decimal(1),
         effect() {
-            return player[this.layer].points.add(1.2).pow(0.011).min(1).max(100)
+            return player[this.layer].points.total.max(1).add(1.7).pow(0.08).max(1).min(100)
         },
         effectDisplay() {
             let capped = upgradeEffect(this.layer, this.id).gte(100) ? "(Capped)" : "";
@@ -56,7 +56,7 @@ addLayer("c", {
         description: "Crystals boosts infects",
         cost: new Decimal(5),
         effect() {
-            return player[this.layer].points.add(1.5).pow(0.013).min(1).max(50)
+            return player[this.layer].points.total.max(1).add(1.3).pow(0.14).max(1).min(50)
         },
         effectDisplay() {
             let capped = upgradeEffect(this.layer, this.id).gte(50) ? "(Capped)" : "";
@@ -72,7 +72,7 @@ addLayer("c", {
         description: "Crystals boosts infects",
         cost: new Decimal(15),
         effect() {
-            return player[this.layer].points.add(1.2).pow(0.11).min(1).max(100)
+            return player[this.layer].points.total.max(1).add(1.6).pow(0.15).max(1).min(100)
         },
         effectDisplay() {
             let capped = upgradeEffect(this.layer, this.id).gte(100) ? "(Capped)" : "";
