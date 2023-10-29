4fe96a27c6f72199432a14c35cc1c10626c04833
Acythe3 <marcusjacksondred@gmail.com>
2023-10-29 17:28:38 -0400

    Update c.js

--- js/layers/c.js
+++ js/layers/c.js
@@ -12,7 +12,7 @@ addLayer("c", {
     baseResource: "infects", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: .94, // Prestige currency exponent
+    exponent: .85, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
