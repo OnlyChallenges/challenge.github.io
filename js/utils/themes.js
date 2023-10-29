c26e7cf7eb1301a46fe22748e985d296fc995931
Acythe3 <marcusjacksondred@gmail.com>
2023-10-29 19:35:38 -0400

    Update themes.js

--- js/utils/themes.js
+++ js/utils/themes.js
@@ -7,7 +7,7 @@ var colors = {
 		1: "#ffffff",//Branch color 1
 		2: "#bfbfbf",//Branch color 2
 		3: "#7f7f7f",//Branch color 3
-		color: "#dfdfdf",
+		color: "#fabbb6",
 		points: "#ffffff",
 		locked: "#bf8f8f",
 		background: "#0f0f0f",
