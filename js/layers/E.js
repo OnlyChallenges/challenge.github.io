dd0675d228b05a38bc9f4c78071b68e0ab6799f2
Acythe3 <marcusjacksondred@gmail.com>
2023-10-29 02:19:25 -0400

    Expo Update

--- js/layers/E.js
+++ js/layers/E.js
@@ -12,7 +12,7 @@ addLayer("E", {
     baseResource: "crystals", // Name of resource prestige is based on
     baseAmount() {return player["c"].points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.33, // Prestige currency exponent
+    exponent: 0.62, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
