9e02a8387117ce740e908372f3ab2d3c5f191573
Acythe3 <marcusjacksondred@gmail.com>
2023-10-29 19:12:39 -0400

    Update themes.js

--- js/utils/themes.js
+++ js/utils/themes.js
@@ -32,20 +32,21 @@ function changeTheme() {
 	document.body.style.setProperty('--color', colors_theme["color"]);
 	document.body.style.setProperty('--points', colors_theme["points"]);
 	document.body.style.setProperty("--locked", colors_theme["locked"]);
-	}
+}
 function getThemeName() {
 	return options.theme? options.theme : "default";
 }
+
 function switchTheme() {
-let index = themes.indexOf(options.theme)
-if (options.theme === null || index >= themes.length-1 || index < 0) {
-	options.theme = themes[0];
+	let index = themes.indexOf(options.theme)
+	if (options.theme === null || index >= themes.length-1 || index < 0) {
+		options.theme = themes[0];
+	}
+	else {
+		index ++;
+		options.theme = themes[index];
+		options.theme = themes[1];
+	}
+	changeTheme();
+	resizeCanvas();
 }
-else {
-	index ++;
-	options.theme = themes[index];
-	options.theme = themes[1];
-}
-}
-changeTheme();
-resizeCanvas();
