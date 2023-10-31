39166da8b3245254425c98c2d4cd6fa216f338c2
E205-Beta <127055307+E205-Beta@users.noreply.github.com>
2023-10-31 08:45:46 -0400

    Reupdate Challenge

--- js/layers/c.js
+++ js/layers/c.js
@@ -40,9 +40,8 @@ addLayer("c", {
     doReset(resettingLayer) {
         let keep = [];
         if (hasMilestone ('F', 11) && resettingLayer=="F") keep.push("upgrades")
-        if (hasMilestone ('E', 11) && resettingLayer=="E") keep.player.C.upgrades.push(11, 12, 13, 14, 15, 21, 22, 23, 24, 25, 31, 32, 33, 34, 35)
-        if (inChallenge('E',11)) return keep=false
-        if (hasChallenge('E',11)) return keep=true
+        if (hasMilestone ('E', 11) && resettingLayer=="E") keep.player.c.upgrades.push(11, 12, 13, 14, 15, 21, 22, 23, 24, 25, 31, 32, 33, 34, 35)
+        if (inChallenge('E',11)) return false
         if (layers[resettingLayer].row > this.row) layerDataReset(this.layer, keep)
         return keep
     },
