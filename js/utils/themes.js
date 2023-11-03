ef7981b98492c7d3bc073727c0c99423729a0865
Acythe3 <marcusjacksondred@gmail.com>
2023-11-02 20:16:16 -0400

    push1

--- js/utils/themes.js
+++ js/utils/themes.js
@@ -1,12 +1,12 @@
 // ************ Themes ************
-var themes = ["E205"];
-let displayNames = ["E205"];
+var themes = ["E205", "E205_test"];
+let displayNames = ["E205", "E205_test"];
 
 function changeTheme() {
 	document.body.classList = "theme-default " + "theme-" + options.theme;
 }
 function getThemeName() {
-	let index = themes.indexOf(options.theme || "E205");
+	let index = themes.indexOf(options.theme || "default");
 	return displayNames[index];
 }
 
@@ -15,7 +15,12 @@ function switchTheme() {
 
 	if (options.theme === null || index >= themes.length-1 || index < 0) {
 		options.theme = themes[0];
+	} else {
+		index ++;
+		options.theme = themes[index];
+		options.theme = themes[1];
 	}
+	
 	changeTheme();
 	resizeCanvas();
 }
