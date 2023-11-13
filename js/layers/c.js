65f65e2686d6c6f4d63d7884d15e8999b89312a3
e205-idle-beta <104089267+e205-idle-beta@users.noreply.github.com>
2023-11-13 10:23:05 -0500

    keeping

--- js/layers/c.js
+++ js/layers/c.js
@@ -78,7 +78,7 @@ addLayer("c", {
         if (hasMilestone ('F', 12) && resettingLayer == "F") player.E.milestones.push("11", "12", "13", "14")
         if (hasUpgrade ('F', 16) && resettingLayer == "F") player.E.milestones.push("15")
         if (hasUpgrade ('F', 22) && resettingLayer == "F") player.E.milestones.push("16")
-        if (hasUpgrade('R', 16)) player.R.upgrades("13", "14", "15", "16")
+        if (hasUpgrade('R', 16)) player.R.upgrades.push("13", "14", "15", "16")
         // Human Milestone
         if (hasMilestone ('H', 11) && resettingLayer == "H") player.c.upgrades.push("11", "12", "13", "14", "15", "21", "22", "23", "24", "25", "31", "32", "33", "34", "35","41", "42", "43", "44", "45","51", "52", "53", "54", "55")
         if (hasMilestone ('H', 13) && resettingLayer == "H") player.E.upgrades.push("11","12","13","14","15","16","21","22","23","24","25","26","31","32","33","34","35","36")
