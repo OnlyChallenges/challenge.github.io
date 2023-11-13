7be7294510b2507144de24ca65ebb5ea0d0e90bd
e205-idle-beta <104089267+e205-idle-beta@users.noreply.github.com>
2023-11-13 12:12:41 -0500

    fix

--- js/layers/timeline.js
+++ js/layers/timeline.js
@@ -8,10 +8,10 @@ addLayer("CT", {
         best: new Decimal(0),
     }},
     color: "#ea3add",
-    requires: new Decimal(1e999999), // Can be a function that takes requirement increases into account
-    resource: "weapons", // Name of prestige currency
-    baseResource: "crystals", // Name of resource prestige is based on
-    baseAmount() {return player.c.points}, // Get the current amount of baseResource
+    requires: new Decimal(1e9999), // Can be a function that takes requirement increases into account
+    resource: "Collapsed Timelines", // Name of prestige currency
+    baseResource: "infects", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 1, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
