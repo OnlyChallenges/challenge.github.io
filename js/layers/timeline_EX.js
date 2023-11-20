8882131f30c16f893db18178296697f9d1dbe799
e205-idle-beta <104089267+e205-idle-beta@users.noreply.github.com>
2023-11-20 10:15:37 -0500

    ijo

--- js/layers/timeline_EX.js
+++ js/layers/timeline_EX.js
@@ -143,7 +143,7 @@ upgrades: {
             tooltip: "(sin(infects))+2",
             cost: new Decimal(1.33e11),
             effect() {
-                return (max(1).add(2).sin(player.points)).max(1).min(5);
+                return (add(2).sin(player.points)).max(1).min(5);
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" },
             unlocked(){
