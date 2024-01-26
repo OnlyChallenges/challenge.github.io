bb801fa483b1786eadbbb4aca2448cb3bb661caa
e205-idle-beta <104089267+e205-idle-beta@users.noreply.github.com>
2024-01-26 15:02:20 -0500

    Update layer2.js

--- js/layers/layer2.js
+++ js/layers/layer2.js
@@ -3,7 +3,7 @@ addLayer("bP", {
     symbol: "bP", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
-        unlocked: true,
+        unlocked: false,
 		points: new Decimal(0),
     }},
     color: "#8ADC13",
