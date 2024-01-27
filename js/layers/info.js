93f725d349f55fae5c6e01936ae7847587599d72
e205-idle-beta <104089267+e205-idle-beta@users.noreply.github.com>
2024-01-27 17:15:17 -0500

    Update layers

---
+++ js/layers/info.js
@@ -0,0 +1,16 @@
+addLayer("At", {
+        startData() { return {
+            unlocked: true,
+        }},
+        color: "#2A87cF",
+        row: "side",
+        tabFormat: {
+            "Automation": {
+                content: [
+                    "blank",
+                    "blank",
+                    ],
+            },
+          
+        },
+})
