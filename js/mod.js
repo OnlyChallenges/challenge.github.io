19b77cd2d431632129f294a84b25ae2bbc7fc532
e205-idle-beta <104089267+e205-idle-beta@users.noreply.github.com>
2024-01-27 14:32:11 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -46,6 +46,7 @@ function getPointGen() {
 	if (hasUpgrade('p', 11)) gain = gain.times(2)
 	if (hasUpgrade('p', 13)) gain = gain.times(10)
 if (hasMilestone('bP', 11)) gain = gain.times(3)
+if (hasMilestone('dP', 12)) gain = gain.times(10)
 if (player.b.unlocked) gain = gain.times(tmp.b.effect)
 	return gain
 }
