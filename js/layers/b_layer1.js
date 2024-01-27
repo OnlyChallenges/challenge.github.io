008ac4c860d77a72523c590d5fe5d46fd5bfeb68
e205-idle-beta <104089267+e205-idle-beta@users.noreply.github.com>
2024-01-27 12:02:32 -0500

    Update layers

---
+++ js/layers/b_layer1.js
@@ -0,0 +1,39 @@
+addLayer("b", {
+    name: "boosters", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "B", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+                points: new Decimal(0),
+    }},
+    color: "#4343de",
+    requires(){ 
+        let requirement = new Decimal(50)
+        return requirement
+    },
+    resources: "boosters", // Name of prestige currency
+    baseResource: "buffed prestige points", // Name of resource prestige is based on
+    baseAmount() {return player.bP.points}, // Get the current amount of baseResource
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 2, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        let mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    effect(){
+        let eff = (player.b.points.add(1)).times(player.b.points)
+        return eff
+    },
+    effectDescription() {
+        dis = "which boosts point gain by "+format(tmp.b.effect)+"x"
+        return dis
+    },
+    row: 2, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "B", description: "shift + b: Reset for Boosters", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return hasUpgrade('cP', 13) || player.b.unlocked},  
+})
