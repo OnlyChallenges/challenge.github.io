57400b00b915cf71826771b329e0d7003158e7f1
e205-idle-beta <104089267+e205-idle-beta@users.noreply.github.com>
2024-01-28 08:12:51 -0500

    Update layer5.js

--- js/layers/layer5.js
+++ js/layers/layer5.js
@@ -16,7 +16,7 @@ addLayer("eP", {
     baseResource: "delta prestige points", // Name of resource prestige is based on
     baseAmount() {return player.dP.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.25, // Prestige currency exponent
+    exponent: 0.2, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         let mult = new Decimal(1)
         return mult
