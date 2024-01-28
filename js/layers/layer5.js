fd0d20c5c078f17b0a0a02c0398cbaa4a62ca0c4
e205-idle-beta <104089267+e205-idle-beta@users.noreply.github.com>
2024-01-28 08:59:55 -0500

    Update layer5.js

--- js/layers/layer5.js
+++ js/layers/layer5.js
@@ -19,6 +19,9 @@ addLayer("eP", {
     exponent: 0.2, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         let mult = new Decimal(1)
+        if (player.fP.unlocked) mult = mult.times(tmp.fP.effect)
+        if (hasUpgrade('fP', 11)) mult = mult.times(3)
+        if (hasUpgrade('fP', 13)) mult = mult.times(10)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
