2f658232bab30f9df09f7c18851b5d9a67739018
e205-idle-beta <104089267+e205-idle-beta@users.noreply.github.com>
2024-01-28 08:50:47 -0500

    Update layers

---
+++ js/layers/layer6.js
@@ -0,0 +1,91 @@
+addLayer("fP", {
+    name: "flattened prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "fP", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+                points: new Decimal(0),
+    }},
+    color: "#a00091",
+    requires(){ 
+        let requirement = new Decimal(100)
+        if (hasUpgrade('fP', 12)) requirement = requirement.div(1.5)
+        return requirement
+    },
+    resource: "flattened prestige points", // Name of prestige currency
+    baseResource: "exterior prestige points", // Name of resource prestige is based on
+    baseAmount() {return player.eP.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.35, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        let mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    effect(){
+        let eff = (player.eP.points.add(1)).pow(0.48)
+        return eff
+    },
+    effectDescription() {
+        dis = "which boosts Exterior Prestige Point gain by "+format(tmp.fP.effect)+"x"
+        return dis
+    },
+    passiveGeneration(){
+        let passive = new Decimal(0)
+        return passive
+    },
+    row: 5, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "f", description: "f: Reset for flattened prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return hasUpgrade('eP', 13) || player.fP.unlocked},
+        milestones: {
+        11: {
+            requirementDescription: "1 Flattened Prestige Points",
+            effectDescription: `Keep content before Exterior Prestige Points on Flattened Prestige Reset<br>Passively gain 10% CP Points and it'll ten-fold the effect every new (layer)P`,
+            done() { return player.fP.points.gte(1) },
+        },
+        12: {
+            requirementDescription: "5 Flattened Prestige Points",
+            effectDescription: `Triple Delta Prestige Points & Ten-Fold Buffed Prestige Point Gain`,
+            done() { return player.fP.points.gte(5) },
+        },
+        13: {
+            requirementDescription: "12 Exterior Prestige Points",
+            effectDescription: `Double Delta Prestige Point Gain`,
+            done() { return player.fP.points.gte(12) },
+        },
+        14: {
+            requirementDescription: "25 Exterior Prestige Points",
+            effectDescription: `Automate Boosters and they don't reset anything`,
+            done() { return player.fP.points.gte(25) },
+        },
+    },
+    upgrades: {
+        11: {
+            title: "Flattened Modern",
+            description: "Double Exterior Prestige point gain",
+            cost: new Decimal(2),
+        },
+        12: {
+            title: "Flattened Normality",
+            description: "/1.5 Flattened Prestige Point requirement",
+            cost: new Decimal(6),
+            unlocked(){ return hasUpgrade('fP', 11) },
+        },
+        13: {
+            title: "Flattened Difference",
+            description: "Two-fold Exterior Prestige Point gain",
+            cost: new Decimal(10),
+            unlocked(){ return hasUpgrade('fP', 12) },
+        },
+        14: {
+            title: "Flattened Association",
+            description: "Twenty-fold Prestige Point gain",
+            cost: new Decimal(15),
+            unlocked(){ return hasUpgrade('fP', 13) },
+        },
+    },
+})
