257a694b02024e996d8fcc87d51826dd88aeec78
e205-idle-beta <104089267+e205-idle-beta@users.noreply.github.com>
2024-01-28 09:02:09 -0500

    Update layer3.js

--- js/layers/layer3.js
+++ js/layers/layer3.js
@@ -39,6 +39,7 @@ addLayer("cP", {
     },
     passiveGeneration(){
         let passive = new Decimal(0)
+        if (hasMilestone('fP', 11)) passive = new Decimal(0.1)
         return passive
     },
     branches: ["dP"],
