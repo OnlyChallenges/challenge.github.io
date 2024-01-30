22889df534ed40b6c865e9705338140d3662b19d
e205-idle-beta <104089267+e205-idle-beta@users.noreply.github.com>
2024-01-30 15:08:33 -0500

    guh just take this ig

--- js/layers/b_layer1.js
+++ js/layers/b_layer1.js
@@ -15,7 +15,7 @@ addLayer("b", {
     baseResource: "buffed prestige points", // Name of resource prestige is based on
     baseAmount() {return player.bP.points}, // Get the current amount of baseResource
     type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 2, // Prestige currency exponent
+    exponent: 1.7, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         let mult = new Decimal(1)
         return mult
