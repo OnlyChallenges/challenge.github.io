b25cb9a518454d88cfe96f2c9329d0ecdc7a2aa9
e205-idle-beta <104089267+e205-idle-beta@users.noreply.github.com>
2024-01-30 12:54:54 -0500

    uh

--- js/layers/playerinfo.js
+++ js/layers/playerinfo.js
@@ -133,7 +133,7 @@ addLayer("PG", {
                     ["display-text",
                     function() {
                       let display = ""
-                      if (player.g.unlocked) display = "Generators Power (Gp): "+ formatWhole(player.g.power) + "(Gp Effect: " + format(tmp.g.powerEff)+"x Prestige Point Gain)"
+                      if (player.g.unlocked) display = "Generators Power (Gp): "+ format(player.g.power) + " (Gp Effect: " + format(tmp.g.powerEff)+"x Prestige Point Gain)"
                       return display
                     },
                     {}],
