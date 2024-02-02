dc6a305e2978ac6844fb8880c025bb78bf94c63a
e205-idle-beta <104089267+e205-idle-beta@users.noreply.github.com>
2024-02-02 08:54:09 -0500

    da

--- js/layers/Ascend_b.js
+++ js/layers/Ascend_b.js
@@ -1,8 +1,8 @@
 addLayer("Ab", {
     name: "Ascension Upgrader", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "Ab", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-    color: "#fc3535",
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    color: "#ff5535",
     requires(){ 
         let requirement = new Decimal(150)
         let nerf = (player.A.points).pow(0.8)
