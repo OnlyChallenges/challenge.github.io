b0f87e31039f0beeb3f6c245ab32fe54a3490754
e205-idle-beta <104089267+e205-idle-beta@users.noreply.github.com>
2024-02-02 19:27:35 -0500

    Update layer4.js

--- js/layers/layer4.js
+++ js/layers/layer4.js
@@ -53,7 +53,7 @@ addLayer("dP", {
         if (hasMilestone('jP', 11)) passive = new Decimal(100)
         if (player.A.unlocked) passive = new Decimal(1000)
         if (player.A.unlocked) passive = passive.plus(tmp.A.passiveEff.div(100))
-        if (inChallenge('Ab', 11)) passive = new Decimal(0)
+        if (inChallenge('Ab', 11)) passive = passive.minus(tmp.A.passiveEff.div(100))
         return passive
     },
     branches: ['eP', 'g'],
