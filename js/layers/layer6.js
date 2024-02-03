24907c8c23f12f30287dab7e0a08537e8e07f6ce
e205-idle-beta <104089267+e205-idle-beta@users.noreply.github.com>
2024-02-02 19:42:06 -0500

    Update layer6.js

--- js/layers/layer6.js
+++ js/layers/layer6.js
@@ -50,6 +50,7 @@ addLayer("fP", {
         if (player.A.unlocked) passive = new Decimal(10)
         if (player.A.unlocked) passive = passive.plus(tmp.A.passiveEff.div(100))
         if (inChallenge('Ab', 11)) passive = passive.minus(tmp.A.passiveEff.div(100))
+        if (inChallenge('Ab', 11)) passive = passive.pow(0.2)
         return passive
     },
     branches: ["gP",'g'],
