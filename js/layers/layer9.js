ae0bb7d32b4fdab7a7f44744a76af32770f3f93f
e205-idle-beta <104089267+e205-idle-beta@users.noreply.github.com>
2024-02-02 19:28:45 -0500

    Update layer9.js

--- js/layers/layer9.js
+++ js/layers/layer9.js
@@ -42,7 +42,7 @@ addLayer("iP", {
     passiveGeneration(){
         let passive = new Decimal(0)
         if (player.A.unlocked) passive = passive.plus(tmp.A.passiveEff.div(100))
-        if (inChallenge('Ab', 11)) passive = new Decimal(0)
+        if (inChallenge('Ab', 11)) passive = passive.minus(tmp.A.passiveEff.div(100))
         return passive
     },
     branches: ["jP"],
