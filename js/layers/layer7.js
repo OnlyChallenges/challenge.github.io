b71d0bfe7e9e9e8c961a60c382b0c03415d91fff
e205-idle-beta <104089267+e205-idle-beta@users.noreply.github.com>
2024-02-02 20:00:09 -0500

    Update layer7.js

--- js/layers/layer7.js
+++ js/layers/layer7.js
@@ -47,7 +47,7 @@ addLayer("gP", {
         if (player.A.unlocked) passive = new Decimal(1)
         if (player.A.unlocked) passive = passive.plus(tmp.A.passiveEff.div(100))
         if (inChallenge('Ab', 11)) passive = passive.minus(tmp.A.passiveEff.div(100))
-        if (inChallenge('Ab', 11)) passive = passive.pow(0.2)
+        if (inChallenge('Ab', 11)) passive = passive.pow(0.07)
         return passive
     },
     branches: ['hP'],
