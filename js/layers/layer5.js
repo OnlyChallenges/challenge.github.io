7b3b63df9ed9bbc1c2a612a5116f434948cd2abe
e205-idle-beta <104089267+e205-idle-beta@users.noreply.github.com>
2024-02-02 19:41:49 -0500

    Update layer5.js

--- js/layers/layer5.js
+++ js/layers/layer5.js
@@ -52,6 +52,7 @@ addLayer("eP", {
         if (player.A.unlocked) passive = new Decimal(100)
         if (player.A.unlocked) passive = passive.plus(tmp.A.passiveEff.div(100))
         if (inChallenge('Ab', 11)) passive = passive.minus(tmp.A.passiveEff.div(100))
+        if (inChallenge('Ab', 11)) passive = passive.pow(0.2)
         return passive
     },
     branches: ['fP'],
