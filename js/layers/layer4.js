50a48c246fae47da1eef0a2c108918072fcf866c
e205-idle-beta <104089267+e205-idle-beta@users.noreply.github.com>
2024-02-02 19:59:37 -0500

    Update layer4.js

--- js/layers/layer4.js
+++ js/layers/layer4.js
@@ -54,7 +54,7 @@ addLayer("dP", {
         if (player.A.unlocked) passive = new Decimal(1000)
         if (player.A.unlocked) passive = passive.plus(tmp.A.passiveEff.div(100))
         if (inChallenge('Ab', 11)) passive = passive.minus(tmp.A.passiveEff.div(100))
-        if (inChallenge('Ab', 11)) passive = passive.pow(0.2)
+        if (inChallenge('Ab', 11)) passive = passive.pow(0.07)
         return passive
     },
     branches: ['eP', 'g'],
