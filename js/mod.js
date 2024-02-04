80fe406f946aff54839d337b292711735467042a
e205-idle-beta <104089267+e205-idle-beta@users.noreply.github.com>
2024-02-04 15:25:17 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -7,7 +7,7 @@ let modInfo = {
 
 	discordName: "",
 	discordLink: "",
-	initialStartPoints: new Decimal (0), // Used for hard resets and new players
+	initialStartPoints: new Decimal (1), // Used for hard resets and new players
 	offlineLimit: 0.001,  // In hours
 }
 
