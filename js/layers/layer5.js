5909b2874fd3d978da5c2b03c606a085afcfc54c
e205-idle-beta <104089267+e205-idle-beta@users.noreply.github.com>
2024-02-04 12:08:52 -0500

    Update layer5.js

--- js/layers/layer5.js
+++ js/layers/layer5.js
@@ -31,7 +31,6 @@ addLayer("eP", {
         if (inChallenge('Ab', 11)) mult = mult.div(tmp.A.boostEff)
         if (inChallenge('Ab', 11)) mult = mult.pow(0.1)
         if (inChallenge('Ab', 11) && hasUpgrade('Ab', 25)) mult = mult.times(100)
-        if (inChallenge('Ab', 12)) passive = new Decimal(0)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
@@ -54,6 +53,7 @@ addLayer("eP", {
         if (player.A.unlocked) passive = passive.plus(tmp.A.passiveEff.div(100))
         if (inChallenge('Ab', 11)) passive = passive.minus(tmp.A.passiveEff.div(100))
         if (inChallenge('Ab', 11)) passive = passive.pow(0.07)
+        if (inChallenge('Ab', 12)) passive = new Decimal(0)
         return passive
     },
     branches: ['fP'],
