3502c9eec9c162bafedd84977777822ba7a3186e
e205-idle-beta <104089267+e205-idle-beta@users.noreply.github.com>
2024-02-05 20:32:31 -0500

    Update Ascend.js

--- js/layers/Ascend.js
+++ js/layers/Ascend.js
@@ -10,8 +10,10 @@ addLayer("A", {
         if (player.A.points.gte(150)) nerf = (player.A.points.times(player.A.power).times(player.A.passive).times(player.A.boost).times(tmp.A.powerEff).times(tmp.A.passiveEff).times(tmp.A.boostEff).times(1e25)).pow(0.95)
         if (player.A.points.gte(200)) nerf = (player.A.points.times(player.A.power).times(player.A.passive).times(player.A.boost).times(tmp.A.powerEff).times(tmp.A.passiveEff).times(tmp.A.boostEff).times(1e50)).pow(1.2)
         if (player.A.points.gte(250)) nerf = (player.A.points.times(player.A.power).times(player.A.passive).times(player.A.boost).times(tmp.A.powerEff).times(tmp.A.passiveEff).times(tmp.A.boostEff).times(1e100)).pow(1.6)
+        if (player.A.points.gte(350)) nerf = nerf.pow(player.points.log10().pow(0.1))
         if (player.A.points.gte(200)) requirement = requirement.times(nerf.times(nerf))
         if (player.A.points.gte(300)) requirement = requirement.pow(player.A.points.log10().div(1.6))
+
         if (player.A.points.gte(50)) requirement = requirement.times(nerf)
         if (hasChallenge('Ab', 11)) requirement = requirement.pow(0.65)
         if (hasChallenge('Ab', 12)) requirement = requirement.pow(0.37)
@@ -124,6 +126,7 @@ addLayer("A", {
         if (player.A.points.gte(200)) func = 'Ascension Requirement is nerfed by ' + format(tmp.A.requires.div(150)) + 'x (((a*(AP*MP*BP)*(APE*MPE*BPE)*1e50)^1.2))^2<br> Your own Generation is truly against you...'
         if (player.A.points.gte(250)) func = 'Ascension Requirement is nerfed by ' + format(tmp.A.requires.div(150)) + 'x<br> (((a*(AP*MP*BP)*(APE*MPE*BPE)*1e100)^1.6))^2<br>'
         if (player.A.points.gte(300)) func = 'Ascension Requirement is nerfed by ' + format(tmp.A.requires.div(150)) + 'x<br> (((a*(AP*MP*BP)*(APE*MPE*BPE)*1e100)^1.6)^2)^log(a/1.6)<br>'
+        if (player.A.points.gte(350)) func = 'Ascension Requirement is nerfed by ' + format(tmp.A.requires.div(150)) + 'x<br> ((log(points^0.1)^(a*(AP*MP*BP)*(APE*MPE*BPE)*1e100)^1.6)^2)^log(a/1.6)^(AbC_Effects)<br>Will this ever end?<br>'
         return func
     }
     , {}], "blank", ["display-text", function() {
