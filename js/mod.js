09340de5fe5c149a28a37982616b81dfd4b4076d
OnlyChallenges <159181047+OnlyChallenges@users.noreply.github.com>
2024-02-07 18:53:13 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -75,6 +75,7 @@ function getPointGen() {
 	if (inChallenge('d', 12)) gain = gain.div(3)
 	if (inChallenge('d', 14)) gain = gain.div(player.p.points.pow(0.8).add(1))
  if (inChallenge('d', 15)) gain = gain.div(100)
+ if (inChallenge('d', 15)) gain = gain.div(player.points.pow(0.15).add(1))
 	return gain
 }
 
