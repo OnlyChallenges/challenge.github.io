4eac26ed1610f0c2ab3bd3eedf8ffe656abab831
OnlyChallenges <159181047+OnlyChallenges@users.noreply.github.com>
2024-02-06 20:19:42 -0500

    Update challenge.js

--- js/layers/challenge.js
+++ js/layers/challenge.js
@@ -84,7 +84,7 @@ addLayer("p", {
             goalDescription: "50 Points",
             rewardEffect() { return (player.p.points.pow(0.25).add(1))},
             rewardDescription(){ return "Points are boosted by Prestige Points"},
-            rewardDisplay(){return format(challengeEffect('p', 15).floor())+"x"},
+            rewardDisplay(){return format(challengeEffect('p', 15))+"x"},
             unlocked(){
                 let unlock = (hasChallenge('p',14)|| inChallenge('p', 15) || hasChallenge('p', 15))
                 return unlock
@@ -93,12 +93,12 @@ addLayer("p", {
 16: {
             name: "Logful Approach",
             challengeDescription: 
-            `Double log point gain`,
+            `Triple log point gain`,
             canComplete: function() {return player.points.gte(2)},
             goalDescription: "2 Points",
             rewardEffect() { return (player.p.points.pow(0.17).add(1))},
             rewardDescription(){ return "Points are boosted by Prestige Points again"},
-            rewardDisplay(){return format(challengeEffect('p', 16).floor())+"x"},
+            rewardDisplay(){return format(challengeEffect('p', 16))+"x"},
             unlocked(){
                 let unlock = (hasChallenge('p',15)|| inChallenge('p', 16) || hasChallenge('p', 16))
                 return unlock
