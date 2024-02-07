c0c65babd4c230b824113b55a7051b9071163df9
OnlyChallenges <159181047+OnlyChallenges@users.noreply.github.com>
2024-02-07 08:30:35 -0500

    d

--- js/layers/challenge.js
+++ js/layers/challenge.js
@@ -28,6 +28,7 @@ addLayer("p", {
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
+    branches: ["d"],
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
