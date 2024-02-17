d6bf2c97be53421c57e12868d88ea58daa8c1e4e
OnlyChallenges <159181047+OnlyChallenges@users.noreply.github.com>
2024-02-17 14:05:52 -0500

    Update achievements.js

--- js/layers/achievements.js
+++ js/layers/achievements.js
@@ -4,7 +4,7 @@ addLayer("A", {
     startData() { return {
         unlocked: true,
     }},
-    color: rainbow,
+    color: "#5DE18A",
     tooltip: "Achievements",
     row: "side", // Row the layer is in on the tree (0 is the first row)
     layerShown(){return true},
