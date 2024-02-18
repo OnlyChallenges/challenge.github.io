e8dae9788a0d74b78d57da4315899747874568e2
OnlyChallenges <159181047+OnlyChallenges@users.noreply.github.com>
2024-02-18 16:09:17 -0500

    Update custom_test.js

--- js/layers/custom_test.js
+++ js/layers/custom_test.js
@@ -122,7 +122,7 @@ if (player.L.enemyHP <= (0)) click = false
 return click},
             onClick() {
             if (player.L.randomizer == (7))
-                 player.L.enemyHP = player.L.enemyHP.minus(player.L.attack.add(player.L.Wattack.times(2.3).floor())).add(player.L.enemyDefense)
+                 player.L.enemyHP = player.L.enemyHP.minus(player.L.attack.add(player.L.Wattack.times(Math.floor(Math.random() * 7) + 1).floor())).add(player.L.enemyDefense)
                  player.L.health = player.L.health.minus(player.L.enemyAttack)
                  if (player.L.enemyHP <= (0)) player.L.exp = player.L.exp.add(3)
                  if (player.L.enemyHP <= (0)) player.L.randomizer = minus(player.L.randomizer)
