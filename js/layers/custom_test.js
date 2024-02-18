9f4fa6ecbd8477bfafd4ab9491496dafd2a34728
OnlyChallenges <159181047+OnlyChallenges@users.noreply.github.com>
2024-02-18 10:53:03 -0500

    Update custom_test.js

--- js/layers/custom_test.js
+++ js/layers/custom_test.js
@@ -49,7 +49,7 @@ return func
                 "blank",
                 ["display-text",function(){let func = ""
  if (player.L.randomizer.gte(80)) func = "* You Stepped On Some Leaves"
- if (player.L.randomizer = (60, 61, 62, 63, 64, 65, 66, 67. 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79)) func = "* You Stepped On Some Rocks"
+ if (player.L.randomizer = (60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79)) func = "* You Stepped On Some Rocks"
  if (player.L.randomizer = (30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59)) func = "* You Walked Across A Room"
  if (player.L.randomizer.lte(29)) func = "*[Battle_Function1]"
  return func
