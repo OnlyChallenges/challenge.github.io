0454c2b16b58f4367cdd07c1d16091c72f184d65
OnlyChallenges <159181047+OnlyChallenges@users.noreply.github.com>
2024-02-19 10:47:03 -0500

    Update custom_test.js

--- js/layers/custom_test.js
+++ js/layers/custom_test.js
@@ -266,6 +266,7 @@ return click},
                  player.L.defenseRNG = Math.floor((Math.random() * 3) + 1)
                  player.L.enemyHP = player.L.enemyHP.minus(player.L.attack.add(player.L.Wattack.times(player.L.dmgMult))).add(player.L.enemyDefense).max(0)
                  player.L.health = player.L.health.minus(player.L.enemyAttack.minus(player.L.defense.add(player.L.Wdefense.times(player.L.defenseRNG))).max(0))
+                 player.points = player.points.minus(2.5)
                  if (player.L.enemyHP <= (0) && player.L.randomizer == (7) && player.L.zone == (1)) player.L.exp = player.L.exp.add(3)
                  if (player.L.enemyHP <= (0) && player.L.randomizer == (2) && player.L.zone == (1)) player.L.exp = player.L.exp.add(4)
                  if (player.L.enemyHP <= (0) && player.L.randomizer == (6) && player.L.zone == (1)) player.L.exp = player.L.exp.add(2.58)
