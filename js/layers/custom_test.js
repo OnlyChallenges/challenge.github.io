f9ce7b2afb02767e41f050f87ee8ff5d643232c6
OnlyChallenges <159181047+OnlyChallenges@users.noreply.github.com>
2024-02-20 13:12:17 -0500

    refix attempt

--- js/layers/custom_test.js
+++ js/layers/custom_test.js
@@ -130,7 +130,7 @@ addLayer("L", {
 },
 
     colorcheckerTwo(){
-    if ((player.L.randomizer == (7) || player.L.randomizer == (6) || player.L.randomizer == (2)) && (player.L.health >= player.L.healthMax)) player.L.spec = "#11f47f"
+    if ((player.L.randomizer == (7) || player.L.randomizer == (6) || player.L.randomizer == (2)) && (player.L.health <= player.L.healthMax)) player.L.spec = "#11f47f"
     if (player.points > 2.49) player.L.spec = "#11f4f7"
     else player.L.spec = "#f5cbcb"
 },
