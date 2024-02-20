19eb9cc97a62aa8ac213927022f1d9eef7c74072
OnlyChallenges <159181047+OnlyChallenges@users.noreply.github.com>
2024-02-20 06:39:45 -0500

    Update custom_test.js

--- js/layers/custom_test.js
+++ js/layers/custom_test.js
@@ -53,6 +53,8 @@ addLayer("L", {
       if ((player.L.exp > 400) && player.L.level == 10) player.L.exp = new Decimal(0)
       if ((player.L.exp > 500) && player.L.level == 11) player.L.expMax = player.L.expMax.add(350)
       if ((player.L.exp > 500) && player.L.level == 11) player.L.exp = new Decimal(0)
+      if ((player.L.exp > 850) && player.L.level == 12) player.L.expMax = player.L.expMax.add(450)
+      if ((player.L.exp > 850) && player.L.level == 12) player.L.exp = new Decimal(0)
 },
     recheckChecker(){
       if ((player.L.expMax > 10) && player.L.level == 1) player.L.level = new Decimal(2)
@@ -66,6 +68,7 @@ addLayer("L", {
       if ((player.L.expMax > 300) && player.L.level == 9) player.L.level = new Decimal(10)
       if ((player.L.expMax > 400) && player.L.level == 10) player.L.level = new Decimal(11)
       if ((player.L.expMax > 500) && player.L.level == 11) player.L.level = new Decimal(12)
+      if ((player.L.expMax > 850) && player.L.level == 12) player.L.level = new Decimal(12)
       if (player.L.level == 4) player.L.zone = new Decimal(2)
       if (player.L.level == 11) player.L.zone = new Decimal(3)
       if (player.L.level == 1) player.L.healthMax = new Decimal(20)
@@ -97,6 +100,9 @@ addLayer("L", {
       if (player.L.level == 11) player.L.defense = new Decimal(6)
       if (player.L.level == 12) player.L.healthMax = new Decimal(67)
       if (player.L.level == 12) player.L.Wattack = new Decimal(6)
+      if (player.L.level == 13) player.L.healthMax = new Decimal(75)
+      if (player.L.level == 13) player.L.Wattack = new Decimal(8)
+      if (player.L.level == 13) player.L.defense = new Decimal(7)
 },
 
     colorcheckerOne(){
@@ -210,6 +216,7 @@ if ((player.L.exp < 1) && player.L.level == (9)) func = "<levelup>Level Up! You'
 if ((player.L.exp < 1) && player.L.level == (10)) func = "<levelup>Level Up! You're now Level 10</levelup><br> (+7 Max Health, +4 Attack, +1 Defense)<br><red>Don't let them escape...</red>"
 if ((player.L.exp < 1) && player.L.level == (11)) func = "<levelup>Level Up! You're now Level 11</levelup><br> (+4 Max Health, +2 Attack, +2 Defense, +1 Weapon Defense)<br>"
 if ((player.L.exp < 1) && player.L.level == (12)) func = "<levelup>Level Up! You're now Level 12</levelup><br> (+3 Max Health, +2 Weapon Attack)<br>"
+if ((player.L.exp < 1) && player.L.level == (13)) func = "<levelup>Level Up! You're now Level 12</levelup><br> (+8 Max Health, +2 Weapon Attack, +1 Defense)<br>"
 return func
 },{}],
                 "blank",
@@ -426,6 +433,7 @@ if ((player.L.health >= 53) && player.L.level == (9)) click = false
 if ((player.L.health >= 60) && player.L.level == (10)) click = false
 if ((player.L.health >= 64) && player.L.level == (11)) click = false
 if ((player.L.health >= 67) && player.L.level == (12)) click = false
+if ((player.L.health >= 75) && player.L.level == (13)) click = false
 if (player.L.health <= (0)) click = false
 return click},
             onClick() {
