ba93061fcb6875cf6df8b183701cd564c2de2c74
OnlyChallenges <159181047+OnlyChallenges@users.noreply.github.com>
2024-02-21 11:43:05 -0500

    :)

--- js/layers/achievements.js
+++ js/layers/achievements.js
@@ -34,7 +34,48 @@ achievements: {
     13: {
         name: "Experimental Threat",
         done() { return player.L.level == 10 },
-        tooltip: "Reach Level <logic>10</logic<",
+        tooltip: "Reach Level <logic>10</logic>",
+    },
+    14: {
+        name: "More Damage",
+        done() { return player.L.attack > 10 },
+        tooltip: "Have more than 10 Base Attack ",
+    },
+    15: {
+        name: "Powering Damage",
+        done() { return player.L.level == 16 },
+        tooltip: "Reach Level 16",
+    },
+    16: {
+        name: "Defensive Apporach",
+        done() { return player.L.defense == 5},
+        tooltip: "Get 5 Defense or higher",
+    },
+    17: {
+        name: "Good Programming",
+        done() { return player.L.revives == 1},
+        tooltip: "Congrats!<br>You learned about the revive system!",
+    },
+    21: {
+        name: "Beginner's Murder",
+        done() { return player.L.kills > 50 },
+        tooltip: "Have more than 50 Experiment Kills",
+    },
+    22: {
+        name: "Normal Murder",
+        done() { return player.L.kills > 250 },
+        tooltip: "Have more than 250 Experiment Kills",
+    },
+    23: {
+        name: "Experimental Massacre",
+        done() { return player.L.kills > 600 },
+        tooltip: "Have more than 600 Experiment Kills",
+    },
+    24: {
+        name: "<fail>Your best friend</fail>>",
+        done() { return player.L.kills > 1000 },
+        tooltip: "Have more than 1,000 Experiment Kills<br>Reward: 2,000 XP",
     },
 },
+
 })
