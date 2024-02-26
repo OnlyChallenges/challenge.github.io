d404d757acf93840afba1daab24c7e8aba89aabe
OnlyChallenges <159181047+OnlyChallenges@users.noreply.github.com>
2024-02-26 08:30:00 -0500

    test

--- js/layers/custom_test.js
+++ js/layers/custom_test.js
@@ -70,6 +70,12 @@ addLayer("L", {
             display() {
                 if (player.L.health > 0)
                     return "Health: "+ formatWhole(player.L.health) + " / " + formatWhole(player.L.healthMax)
+                if ((player.L.health > 1000) && (player.L.healthMax > 1000))
+                    return "health: "+ format(player.L.health.div(1000)) + " thousand / " + format( player.L.healthMax.div(1000)) + " thousand"
+                if (player.L.healthMax > 1000)
+                    return "health: "+ formatWhole(player.L.health) + " / " + formatWhole (player.L.healthMax.div(1000)) + " thousand"
+                if (player.L.health > 1000)
+                    return "health: "+ format(player.L.health.div(1000)) + " thousand / " + formatWhole(player.L.healthMax)
                 else
                     return "You died."
             },
