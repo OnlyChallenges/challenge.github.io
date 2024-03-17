ea6d6edeb2b553b317ab8e52a47934338b25a282
OnlyChallenges <159181047+OnlyChallenges@users.noreply.github.com>
2024-03-17 19:11:31 -0400

    Update custom_test.js

--- js/layers/custom_test.js
+++ js/layers/custom_test.js
@@ -419,7 +419,7 @@ addLayer("L", {
         if ((player.L.randomizer == (2) && player.L.zone == (6)) && player.L.enemyHP < player.L.enemyHPMax) // Regenerative Attack If Enemy Health is under the max.
             player.L.enemyHP = player.L.enemyHP.add(6)
 
-        if (player.L.health > player.L.healthMax) player.L.health = player.L.health.minus(0.05)
+        if (player.L.health > player.L.healthMax) player.L.health = player.L.health.minus(0.1)
 // Health Depressed
 
         player.L.exp = player.L.exp.add(0.0000000001)
