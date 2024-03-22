9289a0a44ef02260f9034eee7d4fb1cc60af5b0c
OnlyChallenges <159181047+OnlyChallenges@users.noreply.github.com>
2024-03-22 08:50:45 -0400

    ref

--- js/mod.js
+++ js/mod.js
@@ -45,7 +45,7 @@ function getPointGen() {
   if (hasUpgrade('P', 14)) gain = gain.times(upgradeEffect('P', 14))
   if (hasUpgrade('P', 15)) gain = gain.times(1.7)
   if (hasUpgrade('P', 21)) gain = gain.times(upgradeEffect('P', 21))
-  if (player.SP.unlocked) mult = mult.times(tmp.SP.boostEff)
+  if (player.SP.unlocked) gain = gain.times(tmp.SP.boostEff)
   return gain
 }
 
