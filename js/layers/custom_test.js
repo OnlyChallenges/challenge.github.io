fbc1740e9542e2523281236da1154bdf0efe959c
OnlyChallenges <159181047+OnlyChallenges@users.noreply.github.com>
2024-04-10 11:34:08 -0400

    m

--- js/layers/custom_test.js
+++ js/layers/custom_test.js
@@ -16,9 +16,8 @@ addLayer("P", {
     },
 
     passiveGeneration() {
-        let pass = new Decimal(1)
-        if (!hasUpgrade('V', 13)) pass = new Decimal(0)
-        if (hasUpgrade('V', 13)) pass = pass.add(upgradeEffect('V', 13).minus(1))
+        let pass = new Decimal(0)
+        if (hasUpgrade('V', 13)) pass = pass.add(upgradeEffect('V', 13).div(2))
         return pass
     },
     color: "#FFFFFF",
