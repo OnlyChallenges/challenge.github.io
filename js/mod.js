da9ba2d1052fde46683181e78aaa1e2ab91a699c
OnlyChallenges <159181047+OnlyChallenges@users.noreply.github.com>
2024-04-11 14:41:50 -0400

    yes

--- js/mod.js
+++ js/mod.js
@@ -1,5 +1,5 @@
 let modInfo = {
-  name: format(player.points) + ` particles`,
+  name() { return format(player.points) + `particles`},
   id: "anothermod2",
   author: "vali (snor mimi)",
   pointsName: "particles",
