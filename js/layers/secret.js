48f613a6b82325c08beaf0ddebc2347e4b041d6b
OnlyChallenges <159181047+OnlyChallenges@users.noreply.github.com>
2024-04-12 12:18:15 -0400

    check

--- js/layers/secret.js
+++ js/layers/secret.js
@@ -1,26 +1,34 @@
 addLayer("s", {
-    name: "secret", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "s", // This appears on the layer's node. Default is the id with the first letter capitalized
     startData() {
         return {
             unlocked: true,
             secret: new Decimal(0),
         }
     },
-    type: "side",
-    layerShown: false,
-    clickables:{
+    tabFormat: {
+        "Secret": {
+            content: [
+
+                "blank",
+                "clickables",
+                "blank",
+            ],
+        },
+    },
+    clickables: {
         11: {
             title: "You found a secret",
             display: "Secret",
             canClick: true,
             onClick() {
-                player.s.secret =1 
+                player.s.secret = 1
             },
-            style() {return{
-                'background-color': tmp.W.color,
-            }},
-            unlocked() {return player.s.secret == 0}
+            style() {
+                return {
+                    'background-color': tmp.W.color,
+                }
+            },
+            unlocked() { return player.s.secret == 0 }
         },
     },
 })
