2f78e7ccd5a8e400f5c7ca3010e7e0a07059c51d
OnlyChallenges <159181047+OnlyChallenges@users.noreply.github.com>
2024-12-16 11:25:45 -0500

    Update gambling.js

--- js/layers/gambling.js
+++ js/layers/gambling.js
@@ -5,7 +5,12 @@ addLayer("D", {
     startData() {
         return {
             unlocked: true,
+            correct: false,
+            incorrect: false,
             points: new Decimal(0),
+            number: new Decimal(50),
+            highlowNum: new Decimal(0),
+            simulation: new Decimal(1),
         }
     },
     tooltip: "Game of Chance",
@@ -15,6 +20,16 @@ addLayer("D", {
         return requirement
 
     },
+
+    RollNumber(){
+        player[this.layer].number = Math.floor((Math.random() * 100) + 1)
+        return player[this.layer].number
+    },
+
+    RollHighOrLow(){
+        player[this.layer].highlowNum = Math.floor((Math.random() * 100) + 1)
+        return player[this.layer].highlowNum   
+    }
     layerShown() {
         let shown = true
         return shown
@@ -37,13 +52,20 @@ addLayer("D", {
         content: [
             ["infobox", "lore"],
             ["infobox", "chances"],
+            "blank",
+            "blank",
+            "blank",
+            ["display-text", function() {return "<orion>Simulation " + simulation + "/100</orion>"}],
             ["display-text", function() {
-                `
-                <input type="checkbox">Test</input>
-                `
-            },{}
+                // This will show what the number is current from what you've gotten"
+                let basetext = "High or Low? The Current Number is " + number
+                if (player[this.layer].incorrect) basetext = "That is incorrect! The Hidden Number was " + highlowNum + "! Let's try again...<br>High or Low? The Current Number is " + number + "!"
+                if (player[this.layer].correct) basetext = "You guessed right! The Hidden Number was " + highlowNum + "!<br>High or Low? The Current Number is " + number + "!"
+
+
+            },],
+            ["clickables", [1]],
             ],
-        ],
     },
     
     },
@@ -61,16 +83,78 @@ addLayer("D", {
             title: "Chances",
             body: 
             `
-                Red or Black: 50%<br>
-                Odd or Even: 50%<br>
-                Red & Odd: 25% --- etc.<br>
-                Number (1 - 100): 1%<br>
-                Green: 1%<br>
-                Perfect Outcome (Green, Number, Even/Odd): 0.005%
+                High or Low has a dependancy chance depending on the current number, and the hidden number, ranging from 1%~99%, depending on what side you choose from. It is best to go with the side that has the highest chance.<br>Jackpot is always 1% so it's luck of getting that.<br>At the end of the simulations, I will show a counter of the amount of times of each side has happened.
             `,
 
         }
     },
+
+
+
+
+
+
+
+
+
+
+
+    clickables: {
+        11: {
+            title: "Low Number",
+            display() {
+                let dis = "The Hidden Number is lower than the Base Number<br>(EX: 19 < 50)"
+                return dis
+            },
+            canClick() {
+                let click = true
+                return click
+            },
+            onClick() {
+                player[this.layer].correct = false 
+                player[this.layer].incorrect = false
+                RollHighOrLow()
+                if (player[this.layer].highlowNum < player[this.layer].number) {(Math.floor(player[this.layer].money.times(2)))},
+                if (player[this.layer].highlowNum < player[this.layer].number) {(player[this.layer].correct = true)},
+                if (player[this.layer].highlowNum >= player[this.layer].number) {(Math.floor(player[this.layer].money.div(3)))},
+                if (player[this.layer].highlowNum >= player[this.layer].number) {(player[this.layer].incorrect = true)},
+
+                RollNumber()
+                
+            },
+            style() { return { 'background-color': tmp[this.layer].color, } },
+        },
+        12: {
+            title: "Jackpot!",
+            display() {
+                let dis = "THe Hidden Number is EXACTLY the same as the Base Number<br>(EX: 40 = 40)<br> <special>You'll lose 90% of your money if it's wrong... but get it right and you'll earn ^1.5 back!</special>"
+                return dis
+            }
+            canClick() {
+                let click = true
+                return click
+            },
+            onClick() {
+                player[this.layer].correct = false 
+                player[this.layer].incorrect = false
+                RollHighOrLow()
+                
+                if (player[this.layer].highlowNum == player[this.layer].number) {(Math.floor(player[this.layer].money.pow(1.5)))},
+                if (player[this.layer].highlowNum == player[this.layer].number) {(player[this.layer].correct = true)},
+                if (player[this.layer].highlowNum !== player[this.layer].number) {(Math.floor(player[this.layer].money.times(0.1)))},
+                if (player[this.layer].highlowNum !== player[this.layer].number) {(player[this.layer].incorrect = true)},
+                // Money^1.5 boost
+
+                RollNumber()
+            }
+            style() { return { 'background-color': tmp[this.layer].color}}
+        }
+    },
+
+
+
+
+
     color: "#33AACC",
     baseAmount() { return player.points },
     row: 0, // Row the layer is in on the tree (0 is the first row)
