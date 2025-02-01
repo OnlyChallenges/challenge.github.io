e230304264aa6aa9e5d34440269ca58922bd7b1a
Acythe3 <acythediscord@gmail.com>
2025-01-31 20:54:11 -0500

    ddddddd

--- js/layers/feburary.js
+++ js/layers/feburary.js
@@ -5,6 +5,7 @@ addLayer("F", {
         return {
             unlocked: false,
             shown: false,
+            page: new Decimal(0),
         }
     },
     tooltip: "Feburary 2025 Solstice Studio Newsletter",
@@ -15,16 +16,19 @@ addLayer("F", {
 
     },
 
+
+    // what the fuck do i do, feburary is tmr and I didn't even start coding the website
+
     layerShown() {
         let shown = player[this.layer].shown
         return shown
     },
 
     update(diff) {
-        if (Febtime <= maintime) player[this.layer].shown = true
+        if (Jantime <= maintime) player[this.layer].shown = true
         else player[this.layer].shown = true
 
-        if (Febtime <= maintime) player[this.layer].unlocked = true
+        if (Jantime <= maintime) player[this.layer].unlocked = true
         else player[this.layer].unlocked = false
         // If the Date is Janurary 1st, 2025; set the Newsletter being opened to true, else keep the newsletter locked until Janurary 1st, 2025
         // Updates every 1/60th of a second.
@@ -33,15 +37,9 @@ addLayer("F", {
 
     nodeStyle() {
         return {
-            "background": (player.F.unlocked || canReset("F")) ? "radial-gradient(#77CCDD, #492312)" : "#bf8f8f",
+            "background": "radial-gradient(circle at bottom, rgb(64, 168, 4) 0, darkgreen 100%)",
+	        "border": "1.5px dashed red",
         }
-    },
-    componentStyles: {
-        "prestige-button": {
-            background() {
-                return (canReset("F")) ? "radial-gradient(#33AACC, #1703fc)" : "#bf8f8f"
-            },
-        },
     },
     color: "#33AACC",
     resource: "Floor 1",
@@ -61,10 +59,98 @@ addLayer("F", {
         let exp = new Decimal(1)
         return exp
     },
-    hotkeys: [
-        { key: "1", description: "1: Reset for Floor 1", onPress() { if (canReset(this.layer) && player.F1.unlocked) doReset(this.layer) } },
-    ],
     doReset(resettingLayer) {
         if (layers[resettingLayer].row > this.row) layerDataReset(this.layer);
+    },
+
+
+    tabFormat:
+        function () {
+            let content =
+
+                [
+                    ["display-text",
+                        `
+                <text style='color:orange ; font-size: 24px'>Solstice Studio Newsletter</text><br>
+                <text style='color:lightgrey ; font-size: 18px'><special>Feburary 2025</special></text><br><br>
+                `,
+                    ],
+                    "blank",
+                    "h-line",
+                    "blank",
+                    ["display-text", `
+                <text style='color:red ; font-size: 24px'>The Final Update</text><br>
+                <text style='color:lightgrey ; font-size: 18px'><special>Loss of Motivation</special></text><br><br>
+                Currently, the game is going into a process of decay<br>I've started to lose my motivation to progress on the game, but that doesn't mean I haven't finished<br>nor quit the final update<br>
+                The Vent System has finally been finished, same with fixing the Crystals by removing camping<br> ontop of them by adding passive damage, no matter what you are, experiment or not<br>
+                The UI for the players have been changed and improved, same with music & events entirely<br><text style='color:yellow'>(Two moons will be the end of human advantage)</text><br>
+                It's kind of an surprise to see that the game had alot of potential,<br>to only a couple of words that shouldn't have been said. 
+                `],
+                    "blank",
+                    "h-line",
+                    "blank",
+                    ["clickable", 12]];
+
+            if (player["F"].page == 1) content = [
+                "blank",
+                "h-line",
+                "blank",
+                ["display-text", `
+                <text style='font-size: 24px'><server>Update Progress</server></text><br>
+                <text style='color:cyan'>Underwater Area: 100% Complete</text><br>
+                <text style='color:blue'>Experiment & Human Spawns: 100% Complete</text><br>
+                <text style='color:orange'>Vent System Implimented: 60% Complete</text><br>
+                <text style='color:lime'>Assist System: ??% Complete</text><br>
+                <text style='color:red'>Intergration of New Chat UI: 80% Complete</text><br>
+                <text style='color:purple'>New Infection System: 39% Complete</text><br>
+                <text style='color:yellow'>New Moon Event: 0% Complete</text><br><br><br>
+                <ruins>Total Bugs Fixed Through This Update: 44</ruins><br>
+                I know there isn't a-lot, sorry.
+                `],
+                "blank",
+                "h-line",
+                "blank",
+                ["clickable", 11]];
+            return content
+        },
+
+
+
+
+
+    // Im way to fucking lazy to recode this
+    clickables: {
+        11: {
+            title: "Go back",
+            canClick() {
+                let click = true
+                if (player[this.layer].page <= 0) click = false
+                return click
+            },
+            onClick() {
+                player[this.layer].page = player[this.layer].page.minus(1)
+            },
+            style() {
+                return {
+                    'background-color': tmp.F.color,
+                }
+            },
+        },
+        12: {
+            title: "Next",
+            canClick() {
+                let click = true
+                if (player[this.layer].page >= 1) click = false
+                return click
+            },
+            onClick() {
+                player[this.layer].page = player[this.layer].page.add(1)
+            },
+            style() {
+                return {
+                    'background-color': tmp.F.color,
+                }
+            },
+        },
     },
 })
--- js/mod.js
+++ js/mod.js
@@ -155,38 +155,35 @@ var displayThings = [
   //   if (modInfo.End == 1) base = ''
   //   return base
   // },
-  function() {
-    return "Next Newsletter Update"
-  },
-  function () {
-    var x = setInterval(function () {
+  // function () {
+  //   var x = setInterval(function () {
 
-      // Get today's date and time
-      var now = new Date().getTime();
+  //     // Get today's date and time
+  //     var now = new Date().getTime();
 
-      // Find the distance between now and the count down date
-      var distance = countDownDate - now;
+  //     // Find the distance between now and the count down date
+  //     var distance = countDownDate - now;
 
 
-      // Time calculations for days, hours, minutes and seconds
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  //     // Time calculations for days, hours, minutes and seconds
+  //     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  //     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  //     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  //     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-      // Display the result in the element with id="demo"
-      document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
+  //     // Display the result in the element with id="demo"
+  //     document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  //       + minutes + "m " + seconds + "s ";
 
-      // If the count down is finished, write some text
-      if (Febtime <= maintime) {
-        document.getElementById("demo").innerHTML = ""
-      }
-    }, 100);
-    let a = `<power><text id='demo' style='font-size:38px'></text></power>`
-    if (modInfo.End == 0) return a = ''
-    return a
-  },
+  //     // If the count down is finished, write some text
+  //     if (Febtime <= maintime) {
+  //       document.getElementById("demo").innerHTML = ""
+  //     }
+  //   }, 100);
+  //   let a = `<power><text id='demo' style='font-size:38px'></text></power>`
+  //   if (modInfo.End == 0) return a = ''
+  //   return a
+  // },
 ]
 
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
