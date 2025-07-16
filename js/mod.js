a840f157d46f6077616ce41a758387f64d0d953e
Acythe3 <acythediscord@gmail.com>
2025-07-15 22:30:06 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -2,7 +2,7 @@
 
 let modInfo = {
   name: `The clock is ticking...`,
-  id: "clock",
+  id: "finale",
   author: "vali (snor mimi)",
   pointsName: "money",
   modFiles: ["tree.js", "layers/newspaper.js"],
